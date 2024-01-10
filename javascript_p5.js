ff793961daa1c6e85df611c1f5bebd9524b385b3
fortunatoj <fortunatoj@pseguros.com.ar>
2024-01-10 11:32:45 -0300

    correccion de bugs

--- javascript_p5.js
+++ javascript_p5.js
@@ -7,3 +7,17 @@ function mostrarPopup() {
 function cerrarPopup() {
   document.getElementById("popup").style.display = "none";
 }
+
+function cerrarOtrosDesplegables(idActual) {
+  // Selecciona todos los elementos con la clase 'collapse' (desplegables)
+  var desplegables = document.querySelectorAll(".collapse");
+
+  // Itera sobre cada desplegable
+  desplegables.forEach(function (desplegable) {
+    // Comprueba si el desplegable actual no es el mismo que el que se está abriendo
+    if (desplegable.id !== idActual) {
+      // Si no es el mismo, remueve la clase 'show' para cerrarlo
+      desplegable.classList.remove("show");
+    }
+  });
+}
